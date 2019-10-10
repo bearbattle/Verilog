4aa6036b37ca9d9f11169ec379b325f8d9fbe40f
bearbattle <chao4150443@gmail.com>
2019-10-10 11:05:50 +0800

    完成了Pipeline.v

--- pipeline.v
+++ pipeline.v
@@ -6,4 +6,17 @@ module pipeline(
     input [31:0] B2,
     output reg [31:0] C = 0);
 
+reg [31:0] buff[1:0];
+integer i;
+
+initial begin
+    for(i = 0; i < 2; i++)
+        buff[i] = 0;
+end
+
+always @(posedge clk) begin
+    buff[0] <= A1 * B1;
+    buff[1] <= A2 * B2;
+    C <= buff[0] + buff[1];
+end
 endmodule
