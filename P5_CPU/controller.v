01bb2d3328c8dde5b6a5c7bfd60f17aafee3e9eb
bearbattle <chao4150443@gmail.com>
2019-11-20 09:37:43 +0800

    分布式控制器

--- P5_CPU/controller.v
+++ P5_CPU/controller.v
@@ -1,19 +1,5 @@
 `timescale 1ns / 1ps
 
-module controller(input [5:0] opcode,
-                  input [5:0] funct,
-                  input Zero,
-                  output reg isb,
-                  output reg isjal,
-                  output reg isjr,
-                  output reg RFWr,
-                  output reg [1:0] WDSel,
-                  output reg DMWr,
-                  output reg [1:0] DstSel,
-                  output reg [1:0] ALUOp,
-                  output reg ALUBSel,
-                  output reg [1:0] EXTOp);
-
 parameter ADD = 2'b00;
 parameter SUB = 2'b01;
 parameter OR = 2'b10;
@@ -22,109 +8,116 @@ parameter UE = 2'b00;
 parameter SE = 2'b01;
 parameter HE = 2'b10;
 
-localparam R   = 6'b000000;
-localparam ori = 6'b001101;
-localparam lw  = 6'b100011;
-localparam sw  = 6'b101011;
-localparam beq = 6'b000100;
-localparam lui = 6'b001111;
-localparam jal = 6'b000011;
+parameter R   = 6'b000000;
+parameter ori = 6'b001101;
+parameter lw  = 6'b100011;
+parameter sw  = 6'b101011;
+parameter beq = 6'b000100;
+parameter lui = 6'b001111;
+parameter jal = 6'b000011;
 
-localparam addu = 6'b100001;
-localparam subu = 6'b100011;
-localparam jr   = 6'b001000;
+parameter addu = 6'b100001;
+parameter subu = 6'b100011;
+parameter jr   = 6'b001000;
 
-always @(*) begin
-    isb = 0;
-    isjal = 0;
-    isjr = 0;
-    RFWr = 0;
-    WDSel = 0;
-    DMWr = 0;
-    DstSel = 0;
-    ALUOp = 0;
-    ALUBSel = 0;
-    EXTOp = 0;
-    case(opcode)
-        R: begin
-            case (funct)
-                addu: begin
-                    RFWr = 1;
-                end
-                subu: begin
-                    RFWr = 1;
-                    ALUOp = SUB;
-                end
-                jr: begin
-                    isjr = 1;
-                end
-                default: begin
-                    isb = 0;
-                    isjal = 0;
-                    isjr = 0;
-                    RFWr = 0;
-                    WDSel = 0;
-                    DMWr = 0;
-                    DstSel = 0;
-                    ALUOp = 0;
-                    ALUBSel = 0;
-                    EXTOp = 0;
-                end
-            endcase
-        end
+module DController(
+    input [31:0] IR,
+    output [1:0] EXTOp
+);
 
-        ori: begin
-            RFWr = 1;
-            DstSel = 1;
-            ALUBSel = 1;
-            ALUOp = OR;
-        end
+wire opcode = IR[31:26];
 
-        lw : begin
-            RFWr = 1;
-            DstSel = 1;
-            WDSel = 1;
-            ALUBSel = 1;
-            EXTOp = SE;
-        end
+assign EXTOp = opcode == lui ? HE :
+                opcode == ori ? UE : SE;
 
-        sw : begin
-            ALUBSel = 1;
-            DMWr = 1;
-            EXTOp = SE;
-        end
+endmodule
 
-        beq: begin
-            isb = Zero;
-        end
+module EController(
+    input [31:0] IR,
+    output [1:0] ALUOp,
+    output BSel
+);
 
-        lui: begin
-            DstSel = 1;
-            RFWr = 1;
-            WDSel= 2;
-            EXTOp = HE;
-        end
+wire opcode = IR[31:26];
+wire funct = IR[5:0];
 
-        jal: begin
-            isjal = 1;
-            RFWr = 1;
-            DstSel = 2;
-            WDSel = 3;
-        end
+assign ALUOp = (opcode == lui) || (opcode == ori) ? OR :
+               (opcode != R) ? ADD : 
+               (funct == SUB) ? SUB : ADD;
 
-        default: begin
-            isb = 0;
-            isjal = 0;
-            isjr = 0;
-            RFWr = 0;
-            WDSel = 0;
-            DMWr = 0;
-            DstSel = 0;
-            ALUOp = 0;
-            ALUBSel = 0;
-            EXTOp = 0;
-        end
-    endcase
-end
+assign BSel = (opcode == lui) || (opcode == ori) || (opcode == lw) || (opcode == sw) ? 1 : 0;
+
+endmodule
+
+module MController(
+    input [31:0] IR,
+    output DMWr
+);
+
+wire opcode = IR[31:26];
+
+assign DMWr = opcode == sw ? 1 : 0;
+
+endmodule
+
+module WController(
+    input [31:0] IR,
+    output RFWr,
+    output [1:0] WRSel,
+    output [1:0] WDSel
+);
+
+wire opcode = IR[31:26];
+wire funct = IR[5:0];
+
+assign RFWr = (opcode == lui) || (opcode == ori) || (opcode == lw) || (opcode == R && funct != jr) || (opcode == jal) ? 1 : 0;
+assign WRSel = (opcode == lw) || (opcode == ori) || (opcode == lui) ? 0 :
+                (opcode == jal) ? 2 : 1;
+assign WDSel =  (opcode == lw) ? 0 :
+                (opcode == jal) ? 2 : 1;
+
+endmodule
+
+module StallController(
+    input [31:0] IRD,
+    input [31:0] IRE,
+    input [31:0] IRM,
+    output enPC,
+    output enD,
+    output FlushE
+);
+
+wire stall_b, stall_b_c_r, stall_b_c_i, stall_b_l;
+wire stall_c_r
+wire stall_c_i
+wire stall_l
+wire stall_s
+
+wire stall
+
+wire [5:0] IRDOP = IRD[31:26];
+wire [5:0] IRDFT = IRD[5:0];
+wire [4:0] IRDRS = IRD[25:21];
+wire [4:0] IRDRT = IRD[20:16];
+wire [4:0] IRDRD = IRD[15:11];
+
+wire [5:0] IREOP = IRE[31:26];
+wire [5:0] IREFT = IRE[5:0];
+wire [4:0] IRERS = IRE[25:21];
+wire [4:0] IRERT = IRE[20:16];
+wire [4:0] IRERD = IRE[15:11];
+
+
+wire [5:0] IRMOP = IRM[31:26];
+wire [5:0] IRMFT = IRM[5:0];
+wire [4:0] IRMRS = IRM[25:21];
+wire [4:0] IRMRT = IRM[20:16];
+wire [4:0] IRMRD = IRM[15:11];
+
+assign stall_b_c_r = (IRDOP == beq) && (IREOP == R) && ((IRDRS == IRERD) || (IRDRT == IRERD));
+assign stall_b_c_i = (IRDOP == beq) && ((IREOP == ori) || (IREOP == lui)) && ((IRDRS == IRERT) || (IRDRT == IRERT));
+assign stall_b_l = (IRDOP == beq) && (IREOP == R) && ((IRDRS == IRERD) || (IRDRT == IRERD));
+
+
 
 endmodule
--- P5_CPU/regs.v
+++ P5_CPU/regs.v
@@ -1,47 +1,26 @@
 `timescale 1ns / 1ps
 
 module regD(
-           input clk,
-           input enD,
-           input FlushD,
-           input [31:0] Instruction,
-           input [31:0] PC4,
-           output reg [5:0] opcode,
-           output reg [5:0] funct,
-           output reg [4:0] rs,
-           output reg [4:0] rt,
-           output reg [4:0] rd,
-           output reg [15:0] imm16,
-           output reg [25:0] imm26,
-           output reg [31:0] PC4D
+        input clk,
+        input enD,
+        input FlushD,
+        input [31:0] IR,
+        input [31:0] PC4,
+        output reg [31:0] IRD,
+        output reg [31:0] PC4D
        );
 initial begin
-    opcode = 0;
-    funct = 0;
-    rs = 0;
-    rt = 0;
-    rd = 0;
-    imm16 = 0;
-    imm26 = 0;
+    IRD = 0;
     PC4D = 0;
 end
 
-always @(posedge clk or posedge FlushD) begin
+always @(posedge clk) begin
     if(enD) begin
-        {opcode, rs, rt, rd} = Instruction[31:11];
-        funct = Instruction[5:0];
-        imm16 = Instruction[15:0];
-        imm26 = Instruction[25:0];
+        IRD = IR;
         PC4D = PC4;
     end
     if(FlushD) begin
-        opcode = 0;
-        funct = 0;
-        rs = 0;
-        rt = 0;
-        rd = 0;
-        imm16 = 0;
-        imm26 = 0;
+        IRD = 0;
         PC4D = 0;
     end
 end
@@ -50,147 +29,98 @@ end
 endmodule
 
 
-    module regE(
-        input clk,
-        input FlushE,
-        input RFWr,
-        input [1:0] RFWDSel,
-        input DMWr,
-        input [1:0] DstSel,
-        input [1:0] ALUOp,
-        input ALUBSel,
-        input [31:0] D32,
-        input [31:0] PC4,
-        input [4:0] rs,
-        input [4:0] rt,
-        input [4:0] rd,
-        input [31:0] RD1,
-        input [31:0] RD2,
-        output reg RFWrE,
-        output reg [1:0] RFWDSelE,
-        output reg DMWrE,
-        output reg DstSelE,
-        output reg [1:0] ALUOpE,
-        output reg ALUBSelE,
-        output reg [31:0] D32E,
-        output reg [31:0] V1,
-        output reg [31:0] V2,
-        output reg [31:0] PC4E
-    );
+module regE(
+    input clk,
+    input FlushE,
+    input [31:0] IR,
+    input [31:0] PC4,
+    input [31:0] RS,
+    input [31:0] RT,
+    input [31:0] D32,
+    output [31:0] IRE,
+    output [31:0] PC4E,
+    output [31:0] RSE,
+    output [31:0] RTE,
+    output [31:0] D32E
+);
 
 initial begin
-    RFWrE = 0;
-    RFWDSelE = 0;
-    DMWrE = 0;
-    DstSelE = 0;
-    ALUOpE = 0;
-    ALUBSelE = 0;
-    D32E = 0;
-    V1 = 0;
-    V2 = 0;
+    IRE = 0;
     PC4E = 0;
+    RSE = 0;
+    RTE = 0;
+    D32E = 0;
 end
 
-always @(posedge clk or posedge FlushE) begin
-    RFWrE = RFWr;
-    RFWDSelE = RFWDSel;
-    DMWrE = DMWr;
-    DstSelE = DstSel;
-    ALUOpE = ALUOp;
-    ALUBSelE = ALUBSel;
-    D32E = D32;
-    V1 = RD1;
-    V2 = RD2;
+always @(posedge clk) begin
+    IRE = IR;
     PC4E = PC4;
+    RSE = RS;
+    RTE = RT;
+    D32E = D32;
     if(FlushE) begin
-        RFWrE = 0;
-        RFWDSelE = 0;
-        DMWrE = 0;
-        DstSelE = 0;
-        ALUOpE = 0;
-        ALUBSelE = 0;
-        D32E = 0;
-        V1 = 0;
-        V2 = 0;
+        IRE = 0;
         PC4E = 0;
+        RSE = 0;
+        RTE = 0;
+        D32E = 0;
     end
 end
 
 endmodule
 
-    module regM(
-        input clk,
-        input RFWr,
-        input [1:0] RFWDSel,
-        input DMWr,
-        input [4:0] Dst,
-        input [31:0] D32,
-        input [31:0] ALUC,
-        input [31:0] V2,
-        input [31:0] PC4,
-        output reg RFWrM,
-        output reg [1:0] RFWDSelM,
-        output reg DMWrM,
-        output reg [4:0] DstM,
-        output reg [31:0] D32M,
-        output reg [31:0] AOM,
-        output reg [31:0] V2M,
-        output reg [31:0] PC4M
-    );
+module regM(
+    input clk,
+    input [31:0] IR,
+    input [31:0] PC4,
+    input [31:0] RT,
+    input [31:0] AO,
+    output [31:0] IRM,
+    output [31:0] PC4M,
+    output [31:0] RTM,
+    output [31:0] AOM
+);
 
 initial begin
-    RFWrM = 0;
-    RFWDSelM = 0;
-    DMWrM = 0;
-    DstM = 0;
-    D32M = 0;
-    AOM = 0;
-    V2M = 0;
+    IRM = 0;
     PC4M = 0;
+    RTM = 0;
+    AOM = 0;
 end
 
 always @(posedge clk) begin
-    RFWrM = RFWr;
-    RFWDSelM = RFWDSel;
-    DMWrM = DMWr;
-    DstM = Dst;
-    D32M = D32;
-    AOM = ALUC;
-    V2M = V2;
+    IRM = IR;
     PC4M = PC4;
+    RTM = RTM;
+    AOM = AO;
 end
 
 endmodule // regM
 
-          module regW(
-              input clk,
-              input RFWr,
-              input [1:0] RFWDSel,
-              input [31:0] D32,
-              input [31:0] AO,
-              input [31:0] V2,
-              input [31:0] PC4,
-              output reg RFWrW,
-              output reg RFWDSelW,
-              output reg [31:0] D32W,
-              output reg [31:0] AOW,
-              output reg [31:0] PC4W
-          );
+module regW(
+    input clk,
+    input [31:0] IR,
+    input [31:0] PC4,
+    input [31:0] DR,
+    input [31:0] AO,
+    output [31:0] IRM,
+    output [31:0] PC4M,
+    output [31:0] DRM,
+    output [31:0] AOM
+);
 
 initial begin
-    RFWrW = 0;
-    RFWDSelW = 0;
-    D32W = 0;
-    AOW = 0;
-    PC4W = 0;
+    IRM = 0;
+    PC4M = 0;
+    DRM = 0;
+    AOM = 0;
 end
 
-always @(posedge clk ) begin
-    RFWrW = RFWr;
-    RFWDSelW = RFWDSel;
-    D32W = D32;
-    AOW = AO;
-    PC4W = PC4;
+always @(posedge clk) begin
+    IRM = IR;
+    PC4M = PC4;
+    DRM = RTM;
+    AOM = AO;
 end
 
 endmodule // regW
